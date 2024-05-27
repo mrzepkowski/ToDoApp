e710805c876b9ea6731271bc80db20402a4aa01e
mrzepkowski <mrzepkowskii@gmail.com>
2024-05-27 22:44:12 +0200

    Remove .DS_Store from everywhere

--- ToDoAppBackend/src/main/java/com/example/ToDoAppBackend/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ToDoAppBackend.repository;
-
-import com.example.ToDoAppBackend.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-}
